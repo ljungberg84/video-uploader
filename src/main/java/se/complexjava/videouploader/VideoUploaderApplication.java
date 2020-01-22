6797de532aadba22c458689781b44dc99089f946
anton <anton.blixter@gmail..com>
2020-01-22 10:12:38 +0100

    Add cors policy

--- src/main/java/se/complexjava/videouploader/VideoUploaderApplication.java
+++ src/main/java/se/complexjava/videouploader/VideoUploaderApplication.java
@@ -3,15 +3,27 @@ package se.complexjava.videouploader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJms
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class VideoUploaderApplication {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(VideoUploaderApplication.class, args);
 	}
-
 }
