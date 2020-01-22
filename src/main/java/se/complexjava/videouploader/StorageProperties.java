bb80eb6f26eba209c33c32e23aad71d91f241998
Carl Ljungberg <ljungberg125@gmail.com>
2020-01-22 14:08:44 +0100

    add stuff

--- src/main/java/se/complexjava/videouploader/StorageProperties.java
+++ src/main/java/se/complexjava/videouploader/StorageProperties.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-
     /**
      * Folder location for storing files
      */
